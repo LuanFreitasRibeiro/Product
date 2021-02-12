903207a03d555883a8043ff90207743550e3b1d7
Luan Freitas <luannr16@gmail.com>
2021-02-12 00:54:08 -0300

    Removing unnecessary files

--- ViewModels/CategoryViewModels/ListCategoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProductCatalog.ViewModels.CategoryViewModels
-{
-    public class ListCategoryViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
