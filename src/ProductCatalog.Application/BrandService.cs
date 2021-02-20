1e3047b7bb150dd6525739a5f0d6837b1370c6df
Luan Freitas <luannr16@gmail.com>
2021-02-20 20:45:23 -0300

    Testing push in same feature

--- src/ProductCatalog.Application/BrandService.cs
+++ src/ProductCatalog.Application/BrandService.cs
@@ -1,4 +1,5 @@
 ﻿using ProductCatalog.Application.Service.Abstraction;
+using ProductCatalog.Application.Validators;
 using ProductCatalog.Domain;
 using ProductCatalog.Repository.Abstraction;
 using ProductCatalog.ViewModels.BrandsViewModels;
@@ -11,18 +12,18 @@ namespace ProductCatalog.Application.Service
     public class BrandService : IBrandService
     {
         private readonly IBrandRepository _brandRepository;
+        private readonly BrandValidator _brandValidator;
 
-        public BrandService(IBrandRepository brandRepository)
+        public BrandService(IBrandRepository brandRepository, BrandValidator brandValidator)
         {
             _brandRepository = brandRepository;
+            _brandValidator = brandValidator;
         }
 
         public async Task<Brand> AddAsync(Brand brand)
         {
-            if (string.IsNullOrEmpty(brand.Name))
-            {
-                throw new ArgumentNullException("The name field is required");
-            }
+            _brandValidator.ValidateIfNameIsNullOrEmpty(brand);
+            //_brandValidator.ValidateIfNameExists(brand);
 
             var obj = new Brand()
             {
---
+++ src/ProductCatalog.Application/Validators/BrandValidator.cs
@@ -0,0 +1,42 @@
+﻿using ProductCatalog.Application.Service.Abstraction;
+using ProductCatalog.Data;
+using ProductCatalog.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductCatalog.Application.Validators
+{
+    public class BrandValidator
+    {
+        private readonly StoreDataContext _context;
+
+        public BrandValidator(Brand brand)
+        {
+        }
+
+        public BrandValidator(StoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public void ValidateIfNameIsNullOrEmpty(Brand brand)
+        {
+            if (string.IsNullOrWhiteSpace(brand.Name))
+            {
+                throw new ArgumentNullException(brand.Name, "Value cannot be null");
+            }
+        }
+
+        public void ValidateIfNameExists(Brand brand)
+        {
+            var result = _context.Brands.Where(x => x.Name == brand.Name);
+
+            if(result != null)
+            {
+                throw new ArgumentNullException(brand.Name, "You already have brand with this name.");
+            }
+        }
+    }
+}
